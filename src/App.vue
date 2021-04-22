8607edd35bf8d7b9a46648c812092d5dd3a87bcd
Kiki Dawson <kikiadawson@gmail.com>
2021-04-22 11:48:45 +0100

    Set up Vue and create newsfeed component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NewsFeed peep="This is the first peep"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewsFeed from './components/NewsFeed.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NewsFeed
   }
 }
 </script>
---
+++ src/components/NewsFeed.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="newsfeed">
+    <h3>This is the news feed</h3>
+    <div class="peeps">
+      <div class="peep"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewsFeed',
+  props: {
+      peeps: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+a {
+  color: #42b983;
+}
+</style>
